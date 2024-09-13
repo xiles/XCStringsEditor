c5d77bc0086fd26c57d61c1f291f0b631db846d1
SpaceGrey <78807862+SpaceGrey@users.noreply.github.com>
2024-09-13 18:17:47 +0800

    fix baidu bugs

--- XCStringsEditor/Translators/BaiduTranslator.swift
+++ XCStringsEditor/Translators/BaiduTranslator.swift
@@ -7,9 +7,35 @@
 
 import Foundation
 class BaiduTranslator: Translator {
+    
+    //https://api.fanyi.baidu.com/doc/21
+    //baidu sucks!
+    private static let languageConvertTable:[String:String]=[
+        "zh-HK":"cht",
+        "zh-TW":"cht",
+        "zh-CN":"zh",
+        "zh-Hans":"zh",
+        "zh-Hant":"cht",
+        "da":"dan",
+        "ko":"kor",
+        "ja":"jp",
+        "es":"spa",
+        "fr":"fra",
+        "fr-CA":"fra",
+        "en-AU":"en",
+        "en-GB":"en",
+        "en-IN":"en",
+        "pt-BR":"pt",
+        "pt-PT":"pt",
+        "es-419":"spa",
+        "vi":"vie"
+    ]
     var translateAPI: any TranslateAPI = BaiduTranslateAPI()
     func translate(_ inputModel: InputModel) async throws -> String {
-        let api = try translateAPI.translate(inputModel)
+        let source = BaiduTranslator.languageConvertTable[inputModel.source] ?? inputModel.source
+        let target = BaiduTranslator.languageConvertTable[inputModel.target] ?? inputModel.target
+        let newModel = InputModel(text: inputModel.text, source: source, target: target)
+        let api = try translateAPI.translate(newModel)
         let result: [String: Any] = try await NetworkManager.request(endpoint: api)
         guard let translations = result["trans_result"] as? [[String: Any]] else {
             throw TranslatorError.responseError
